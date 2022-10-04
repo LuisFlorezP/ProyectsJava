ba238341174e6e5974574a0c19f815adb25eac04
Luis Florez <braian1k3@gmail.com>
2022-10-04 16:13:21 -0500

    Solucionar error del código, ya que al no reiniciar las variables el primero número si tenía su respectiva tabla, pero si deseaba volverlo a intentar con un número diferente, no funcionada ya que brindaba solamente la tabla del número anterior.

--- Tablas.java
+++ Tablas.java
@@ -58,6 +58,8 @@ public class Tablas {
                     }
                     // Imprimir el resultado de toda la tabla de multiplicación 
                     JOptionPane.showMessageDialog(null,"Resultado tablas de multiplicar del: "+numero+".\n   - Resultado "+numero+" X 1: "+t1+"\n   - Resultado "+numero+" X 2: "+t2+"\n   - Resultado "+numero+" X 3: "+t3+"\n   - Resultado "+numero+" X 4: "+t4+"\n   - Resultado "+numero+" X 5: "+t5+"\n   - Resultado "+numero+" X 6: "+t6+"\n   - Resultado "+numero+" X 7: "+t7+"\n   - Resultado "+numero+" X 8: "+t8+"\n   - Resultado "+numero+" X 9: "+t9+"\n   - Resultado "+numero+" X 10: "+t10,"☯☯☯TABLAS DE MULTIPLICAR☯☯☯",JOptionPane.INFORMATION_MESSAGE);
+                    // Reiniciar variables
+                    cont=1; cont2=1; numero=0; resul=0;
                     break;
                 case "2":
                     // Cerrar sistema
