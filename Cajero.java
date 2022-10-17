cc8582f47534939cc6601ec41040620fb03b71ec
Luis Florez <braian1k3@gmail.com>
2022-10-17 12:22:23 -0500

    Generar los respectivos metodos para el programa y continuar puliendo detalles como el saludo de inicio y menú de opciones.

--- Cajero.java
+++ Cajero.java
@@ -17,20 +17,54 @@ public class Cajero {
         int menu=0;
         Cajero valor = new Cajero("Luis Florez","CC","1025882220",cuenta.Ahorros,"105-971-84835");
 
+        JOptionPane.showMessageDialog(null,"♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆\nBIENVENIDO AL PROGRAMA CAJERO\n♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
+
         do {
-            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresar:","LUCHINI S.A.S",JOptionPane.QUESTION_MESSAGE));
+            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Consultar información básica.\n   - Opción 2: Consignaciones.\n   - Opción 3: Retiros.\n   - Opción 4: Consultar saldo.\n   - Opción 5: Saldo con rentabilidad.\n   - Opción 6: Cerrar sistema.","LUCHINI S.A.S",JOptionPane.QUESTION_MESSAGE));
         
             switch (menu) {
-            
+                case 1:
+                    valor.datos();
+                    break;
+                case 2: 
+
+                    break;
+                case 3:
+
+                    break;
+                case 4: 
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"Opción incorrecta, vuelva a intentarlo.","❌❌❌ERROR❌❌❌",JOptionPane.ERROR_MESSAGE);
+                    break;
             }
-        } while ();
-        
-        
-        valor.datos();
+        } while (menu!=6);
     }
 
     void datos() {
         System.out.println("\nINFORMACIÓN BÁSICA DE LA CUENTA.\nNombre titular: "+nombre+"\nTipo documento: "+tipoDoc+"\nNúmero documento: "+documento+"\nTipo de cuenta: "+tipoCuenta+"\nNúmero de cuenta: "+numCuenta+"\n");
     }
 
+    void consignar() {
+
+    }
+
+    void retirar() {
+
+    }
+
+    void saldo() {
+
+    }
+
+    void rentabilidadSaldo() {
+
+    }
 }
