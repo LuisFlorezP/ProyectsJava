13eb2196f52872a1cc761bb59ffe0e015dca6e04
Luis Florez <braian1k3@gmail.com>
2022-10-17 15:31:44 -0500

    Actualizar a dos constructores y mejorar el mensaje de salida de la terminal.

--- Paquetes/Asignatura/Constructor.java
+++ Paquetes/Asignatura/Constructor.java
@@ -1,7 +1,8 @@
 public class Constructor {
     public static void main(String args[]) {
-        Matricula materia = new Matricula("lp1020","Lógica de programación","Ingeniero de sistemas","Alba Vélez","504L",6,6);
-
-        materia.informacion();
+        Matricula materia1 = new Matricula("lp1020","Lógica de programación","Ingeniero de sistemas","Alba Vélez","504L",6,6);
+        Matricula materia2 = new Matricula("lp1020","Lógica de programación",6);
+        materia1.informacion();
+        materia2.informacion();
     }
 }
--- Paquetes/Asignatura/Matricula.java
+++ Paquetes/Asignatura/Matricula.java
@@ -1,23 +1,29 @@
 public class Matricula {
     int numCred, intHora;
-    String codigoA, nomAsig, nomProg, nomDir, codAB;
+    String codigoA, nomAsig, nomProg, docente, codAB;
 
-    Matricula(String codigoA, String nomAsig, String nomProg, String nomDir, String codAB, int numCred, int intHora) {
+    Matricula(String codigoA, String nomAsig, String nomProg, String docente, String codAB, int numCred, int intHora) {
         this.codigoA=codigoA;
         this.nomAsig=nomAsig;
         this.nomProg=nomProg;
-        this.nomDir=nomDir;
+        this.docente=docente;
         this.codAB=codAB;
         this.numCred=numCred;
         this.intHora=intHora;
     }
 
+    Matricula(String codigoA, String nomAsig, int numCred) {
+        this.codigoA=codigoA;
+        this.nomAsig=nomAsig;
+        this.numCred=numCred;
+    }
+
     void informacion() {
-        System.out.println(codigoA);
-        System.out.println(nomAsig);
-        System.out.println(nomProg);
-        System.out.println(nomDir);
-        System.out.println(codAB);
-        System.out.println(numCred);
+        System.out.println("\n - Código asignatura: "+codigoA+".");
+        System.out.println(" - Asignatura: "+nomAsig+".");
+        System.out.println(" - Programa: "+nomProg+".");
+        System.out.println(" - Docente: "+docente+".");
+        System.out.println(" - Bloque y aula: "+codAB+".");
+        System.out.println(" - Créditos: "+numCred+".\n");
     }
 }
