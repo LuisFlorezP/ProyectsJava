d167775740f6661f33f7067465459a928174db05
Luis Florez <braian1k3@gmail.com>
2022-10-16 23:44:45 -0500

    Continuar con la estructura principal del archivo.

--- Cajero.java
+++ Cajero.java
@@ -1,21 +1,31 @@
+import javax.swing.JOptionPane;
 public class Cajero {
     enum cuenta {Ahorros,Corriente}
     String nombre, tipoDoc, documento, numCuenta;
     cuenta tipoCuenta;
-    double saldo;
+    double saldo=0;
 
-    Cajero(String nombre, String tipoDoc, String documento, cuenta tipoCuenta, String numCuenta, double saldo) {
+    Cajero(String nombre, String tipoDoc, String documento, cuenta tipoCuenta, String numCuenta) {
         this.nombre=nombre;
         this.tipoDoc=tipoDoc;
         this.documento=documento;
         this.tipoCuenta=tipoCuenta;
         this.numCuenta=numCuenta;
-        this.saldo=saldo;
     }
 
     public static void main (String args[]) {
-        Cajero valor = new Cajero("Luis Florez","CC","1025882220",cuenta.Ahorros,"105-971-84835",100000);
+        int menu=0;
+        Cajero valor = new Cajero("Luis Florez","CC","1025882220",cuenta.Ahorros,"105-971-84835");
 
+        do {
+            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresar:","LUCHINI S.A.S",JOptionPane.QUESTION_MESSAGE));
+        
+            switch (menu) {
+            
+            }
+        } while ();
+        
+        
         valor.datos();
     }
 
