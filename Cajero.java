1a5d41558d306fe5f76eb83c593456ebb64047b9
Luis Florez <braian1k3@gmail.com>
2022-10-17 15:07:03 -0500

    Finalizar archivo de cajero con sus funciones de consignar, retirar, consultar saldo, información de cuenta y saldo con rentabilidad en caso de aplicar.

--- Cajero.java
+++ Cajero.java
@@ -3,7 +3,7 @@ public class Cajero {
     enum cuenta {Ahorros,Corriente}
     String nombre, tipoDoc, documento, numCuenta;
     cuenta tipoCuenta;
-    double saldo=0;
+    static double saldo=0;
 
     Cajero(String nombre, String tipoDoc, String documento, cuenta tipoCuenta, String numCuenta) {
         this.nombre=nombre;
@@ -20,23 +20,23 @@ public class Cajero {
         JOptionPane.showMessageDialog(null,"♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆\nBIENVENIDO AL PROGRAMA CAJERO\n♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆♆","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
 
         do {
-            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Consultar información básica.\n   - Opción 2: Consignaciones.\n   - Opción 3: Retiros.\n   - Opción 4: Consultar saldo.\n   - Opción 5: Saldo con rentabilidad.\n   - Opción 6: Cerrar sistema.","LUCHINI S.A.S",JOptionPane.QUESTION_MESSAGE));
+            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Consultar información básica.\n   - Opción 2: Consignaciones.\n   - Opción 3: Retiros.\n   - Opción 4: Consultar saldo.\n   - Opción 5: Saldo con rentabilidad.\n   - Opción 6: Cerrar sistema.","♆♆♆CAJERO♆♆♆",JOptionPane.QUESTION_MESSAGE));
         
             switch (menu) {
                 case 1:
                     valor.datos();
                     break;
                 case 2: 
-
+                    valor.consignar(100000);
                     break;
                 case 3:
-
+                    valor.retirar(50000);
                     break;
                 case 4: 
-
+                    valor.saldo();
                     break;
                 case 5:
-
+                    valor.rentabilidadSaldo();
                     break;
                 case 6:
                     JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
@@ -49,22 +49,39 @@ public class Cajero {
     }
 
     void datos() {
-        System.out.println("\nINFORMACIÓN BÁSICA DE LA CUENTA.\nNombre titular: "+nombre+"\nTipo documento: "+tipoDoc+"\nNúmero documento: "+documento+"\nTipo de cuenta: "+tipoCuenta+"\nNúmero de cuenta: "+numCuenta+"\n");
+        JOptionPane.showMessageDialog(null,"INFORMACIÓN BÁSICA DE LA CUENTA.\n   - Nombre titular: "+nombre+"\n   - Tipo documento: "+tipoDoc+"\n   - Número documento: "+documento+"\n   - Tipo de cuenta: "+tipoCuenta+"\n   - Número de cuenta: "+numCuenta,"♆♆♆CAJERO♆♆♆",JOptionPane.INFORMATION_MESSAGE);
     }
 
-    void consignar() {
-
+    void consignar(int nuevo) {
+        saldo+=nuevo;
+        JOptionPane.showMessageDialog(null,"¡CONSIGNACIÓN EXITOSA!\n\nNuevo saldo: "+saldo,"♆♆♆CAJERO♆♆♆",JOptionPane.INFORMATION_MESSAGE);
     }
 
-    void retirar() {
-
+    void retirar(int nuevo) {
+        if (saldo>=nuevo) {
+            saldo-=nuevo;
+            JOptionPane.showMessageDialog(null,"¡RETIRO EXITOSO!\n\nNuevo saldo: "+saldo,"♆♆♆CAJERO♆♆♆",JOptionPane.INFORMATION_MESSAGE);
+        }
+        else {
+            JOptionPane.showMessageDialog(null,"¡SALDO INSUFICIENTE!","♆♆♆CAJERO♆♆♆",JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null,"¡RETIRO FALLIDO!\n\nNuevo saldo: "+saldo,"♆♆♆CAJERO♆♆♆",JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 
     void saldo() {
-
+        JOptionPane.showMessageDialog(null,"Saldo actual: "+saldo,"♆♆♆CAJERO♆♆♆",JOptionPane.INFORMATION_MESSAGE);
     }
 
     void rentabilidadSaldo() {
+        double saldoR=saldo;
+        double renta=0.089;
 
+        if (saldoR>20000) {
+            saldoR+=saldoR*renta;
+            JOptionPane.showMessageDialog(null,"Saldo con rentabilidad: "+saldoR,"♆♆♆CAJERO♆♆♆",JOptionPane.INFORMATION_MESSAGE);
+        }
+        else {
+            JOptionPane.showMessageDialog(null,"Monto insuficiente.","♆♆♆CAJERO♆♆♆",JOptionPane.WARNING_MESSAGE);
+        }
     }
 }
