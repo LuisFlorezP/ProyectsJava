aa5e81c57334d226d569075ef1a04226a8f336bb
Luis Florez <braian1k3@gmail.com>
2022-10-17 16:33:12 -0500

    Agregar un proceso de confirmación de información para poder dar el acceso, el cual se hará con el segundo nombre, con la ayuda de otra opción del equals para saber si es igual sin importar si son las mismas mayúsculas o minúsculas.

--- Sesión.java
+++ Sesión.java
@@ -1,30 +1,39 @@
 import javax.swing.JOptionPane;
 
 public class Sesión {
-    static boolean R;
-    String usuario;
-    String contra;
+    static boolean R1, R2;
+    String usuario, contra, nombre;
 
     Sesión(String usuario, String contra) {
         this.usuario=usuario;
         this.contra=contra;
     }
+    Sesión(String nombre) {
+        this.nombre=nombre;
+    }
 
     public static void main(String args[]) {
         int menu;
-        Sesión datos = new Sesión("LuisFlorez","2004");
+        Sesión datos1 = new Sesión("LuisFlorez","2004");
+        Sesión datos2 = new Sesión("Angel");
 
         JOptionPane.showMessageDialog(null,"★★★★★★★★★★★★★★★★★★\nBIENVENIDO AL SISTEMA GERENCIAL\n★★★★★★★★★★★★★★★★★★","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
-
 
         do {
             menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Acceder.\n   - Opción 2: Cerrar sistema.","★★★GERENCIA★★★",JOptionPane.QUESTION_MESSAGE));
             
             switch (menu) {
                 case 1:
-                    datos.Acceso();
-                    if (R==true) {
+                    datos1.Acceso();
+                    if (R1==true) {
                         JOptionPane.showMessageDialog(null,"¡INICIO DE SESIÓN CORRECTO!","★★★GERENCIA★★★",JOptionPane.INFORMATION_MESSAGE);
+                        datos2.confirmacion();
+                        if (R2==true) {
+                            JOptionPane.showMessageDialog(null,"¡CONFIRMACIÓN CORRECTA!","★★★GERENCIA★★★",JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        else {
+                            JOptionPane.showMessageDialog(null,"¡NOMBRE INCORRECTO!","★★★GERENCIA★★★",JOptionPane.WARNING_MESSAGE);
+                        }
                     }
                     else {
                         JOptionPane.showMessageDialog(null,"¡USUARIO O CONTRASEÑA INCORRECTA!","★★★GERENCIA★★★",JOptionPane.WARNING_MESSAGE);
@@ -47,12 +56,26 @@ public class Sesión {
         contraU=JOptionPane.showInputDialog(null,"Ingresar contraseña de usuario:","★★★GERENCIA★★★",JOptionPane.QUESTION_MESSAGE);
 
         if (usuario.equals(usuarioU)&&contra.equals(contraU)) {
-            R=true;
-            return(R);
+            R1=true;
+            return(R1);
         }
         else {
-            R=false;
-            return(R);
+            R1=false;
+            return(R1);
+        }
+    }
+    boolean confirmacion() {
+        String nombreU;
+
+        nombreU=JOptionPane.showInputDialog(null,"Ingresar segundo nombre para confirmar","★★★GERENCIA★★★",JOptionPane.QUESTION_MESSAGE);
+
+        if (nombre.equalsIgnoreCase(nombreU)) {
+            R2=true;
+            return(R2);
+        }
+        else {
+            R2=false;
+            return(R2);
         }
     }
 }
