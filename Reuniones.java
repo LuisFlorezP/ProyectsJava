2eadd4d4e0a45c957560cddf1f0b479c59a92cf5
Luis Florez <braian1k3@gmail.com>
2022-10-19 08:27:34 -0500

    Colocar horario personal de clases en las opciones de semana.

--- Reuniones.java
+++ Reuniones.java
@@ -7,19 +7,19 @@ public class Reuniones {
         
         switch (reunion) {
             case Lunes: 
-                System.out.println("\nJunta directiva.\n");
+                System.out.println("\nCESDE: 8:15 am - 12:15 pm.\nITM: N/A.\n");
                 break;
             case Martes:
-                System.out.println("\nGrupo primario.\n");
+                System.out.println("\nCESDE: 8:15 am - 11:15 pm.\nITM: 8:00 pm - 10:00 pm.\n");
                 break;
             case Miercoles:
-                System.out.println("\nSeguimiento proyecto gobernación.\n");
+                System.out.println("\nCESDE: N/A.\nITM: 4:00 pm - 6:00 pm. 8:00 pm - 10:00 pm.\n");
                 break;
             case Jueves:
-                System.out.println("\nValidación de requerimientos.\n");
+                System.out.println("\nCESDE: 8:15 am - 11:15 pm.\nITM: 8:00 pm - 10:00 pm.\n");
                 break;
             case Viernes:
-                System.out.println("\nPreparar licitación.\n");
+                System.out.println("\nCESDE: 8:15 am - 12:15 pm.\nITM: 4:00 pm - 6:00 pm. 6:00 pm - 8:00 pm.\n");
                 break;
             default:
                 System.out.println("\nFin de semana.\n");
