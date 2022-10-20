442d25b8a2079a5b3581b904fb1c3a7e5dc58b82
Luis Florez <braian1k3@gmail.com>
2022-10-19 23:38:37 -0500

    Quitar datos para solo trabajar con tres, código, asignatura y programa.

--- Paquetes/Asignatura/Matricula.java
+++ Paquetes/Asignatura/Matricula.java
@@ -1,29 +1,15 @@
 public class Matricula {
-    int numCred, intHora;
-    String codigoA, nomAsig, nomProg, docente, codAB;
+    String codigoA, nomAsig, nomProg;
 
-    Matricula(String codigoA, String nomAsig, String nomProg, String docente, String codAB, int numCred, int intHora) {
+    Matricula(String codigoA, String nomAsig, String nomProg) {
         this.codigoA=codigoA;
         this.nomAsig=nomAsig;
         this.nomProg=nomProg;
-        this.docente=docente;
-        this.codAB=codAB;
-        this.numCred=numCred;
-        this.intHora=intHora;
-    }
-
-    Matricula(String codigoA, String nomAsig, int numCred) {
-        this.codigoA=codigoA;
-        this.nomAsig=nomAsig;
-        this.numCred=numCred;
     }
 
     void informacion() {
         System.out.println("\n - Código asignatura: "+codigoA+".");
         System.out.println(" - Asignatura: "+nomAsig+".");
-        System.out.println(" - Programa: "+nomProg+".");
-        System.out.println(" - Docente: "+docente+".");
-        System.out.println(" - Bloque y aula: "+codAB+".");
-        System.out.println(" - Créditos: "+numCred+".\n");
+        System.out.println(" - Programa: "+nomProg+".\n");
     }
 }
