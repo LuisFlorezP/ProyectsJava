b2aa532389e5cfb7e02dec38784803c3fd08142c
Luis Florez <braian1k3@gmail.com>
2022-10-25 23:25:56 -0500

    Generar una plantilla de aplicación para el arreglo unidimensional utilizado con tipo de dato String.

--- Nombres.java
+++ Nombres.java
@@ -1,5 +1,17 @@
 public class Nombres{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        int ubic;
+        String datos[]=new String[10];
+
+        System.out.print("\nIngresar nombre: ");
+        nombre=sc.nextLine();
+        System.out.print("Ingresar la ubicación para almacenar de 1 a 10: ");
+        ubic=sc.nextInt();
+
+        datos[ubic]=nombre;
+        System.out.println("\nEl arreglo contiene: "+datos[ubic]);
+        System.out.println("\nLongitud del arreglo: "+datos.length+".\n");
     }
 }
