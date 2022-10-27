3e1b113a7226faeec6381c07e6c6be40873f48d6
Luis Florez <braian1k3@gmail.com>
2022-10-27 12:54:40 -0500

    Continuar la estructura de los menús para el programa y crear métodos para realizar los procesos.

--- Nombres.java
+++ Nombres.java
@@ -1,41 +1,65 @@
-import javax.print.attribute.standard.JobHoldUntil;
 import javax.swing.JOptionPane;
 
 public class Nombres{
+    static int menu;
+
     public static void main(String[] args) {
-        String nombre;
-        int ubic, menu;
-        //String datos[]=new String[];
+        int dimen;
 
         JOptionPane.showMessageDialog(null,"⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘\nBIENVENIDO AL PROGRAMA ALMACENAMIENTO\n⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
 
         do {
-            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"","⌘⌘⌘ALMACENAMIENTO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
+            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Almacenamiento de nombres.\n   - Opción 2: Cerrar sistema.","⌘⌘⌘ALMACENAMIENTO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
             switch (menu) {
                 case 1:
-
+                    dimen=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresar el espacio que tendrá el almacenamiento de nombres:","⌘⌘⌘ALMACENAMIENTO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
+                    menu(dimen);
                     break;
                 case 2:
-
-                    break;
-                case 3:
-                JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
-
+                    JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
                     break;
                 default:
                     JOptionPane.showMessageDialog(null,"Opción incorrecta, vuelva a intentarlo.","❌❌❌ERROR❌❌❌",JOptionPane.ERROR_MESSAGE);
                     break;
             }
 
-        } while(menu!=3);
+        } while(menu!=2);
+    }
 
-        System.out.print("\nIngresar nombre: ");
-        nombre=sc.nextLine();
-        System.out.print("Ingresar la ubicación para almacenar de 1 a 10: ");
-        ubic=sc.nextInt();
+    static void menu(int espacio) {
+        String nombres[]=new String[espacio];
 
-        datos[ubic]=nombre;
-        System.out.println("\nEl arreglo contiene: "+datos[ubic]);
-        System.out.println("\nLongitud del arreglo: "+datos.length+".\n");
+        do {
+            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Ingresar nombre.\n   - Opción 2: Ver nombre.\n   - Opción 3: Ver todos los nombres.\n   - Opción 4: Menú anterior.","⌘⌘⌘ALMACENAMIENTO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
+            switch (menu) {
+                case 1:
+                    almacenar();
+                    break;
+                case 2:
+                    dato1();
+                    break;
+                case 3:
+                    dato2();
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"Opción incorrecta, vuelva a intentarlo.","❌❌❌ERROR❌❌❌",JOptionPane.ERROR_MESSAGE);
+                    break;
+            }
+        } while (menu!=4);
+    }
+
+    static void almacenar() {
+
+    }
+
+    static void dato1() {
+
+    }
+
+    static void dato2() {
+
     }
 }
