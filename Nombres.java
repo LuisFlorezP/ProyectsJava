ed5ea29d3ac96460b0970dc9266d8d9e3a0f4dd7
Luis Florez <braian1k3@gmail.com>
2022-10-27 12:30:22 -0500

    Agregar más orden y nueva estructura para el código Nombres.

--- Nombres.java
+++ Nombres.java
@@ -1,9 +1,33 @@
+import javax.print.attribute.standard.JobHoldUntil;
+import javax.swing.JOptionPane;
+
 public class Nombres{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String nombre;
-        int ubic;
-        String datos[]=new String[10];
+        int ubic, menu;
+        //String datos[]=new String[];
+
+        JOptionPane.showMessageDialog(null,"⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘\nBIENVENIDO AL PROGRAMA ALMACENAMIENTO\n⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
+
+        do {
+            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"","⌘⌘⌘ALMACENAMIENTO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
+            switch (menu) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
+
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null,"Opción incorrecta, vuelva a intentarlo.","❌❌❌ERROR❌❌❌",JOptionPane.ERROR_MESSAGE);
+                    break;
+            }
+
+        } while(menu!=3);
 
         System.out.print("\nIngresar nombre: ");
         nombre=sc.nextLine();
