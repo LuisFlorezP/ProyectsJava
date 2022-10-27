aaeb1f0e22817eb9c3ea34b7eb2a5787f9fa48f0
Luis Florez <braian1k3@gmail.com>
2022-10-27 14:43:06 -0500

    Completar los diversos métodos para pedir y almacenar los nombres ingresados por el usuario, además de poder ver cada uno y la totalidad almacenada al mismo tiempo.

--- Nombres.java
+++ Nombres.java
@@ -6,7 +6,7 @@ public class Nombres{
     public static void main(String[] args) {
         int dimen;
 
-        JOptionPane.showMessageDialog(null,"⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘\nBIENVENIDO AL PROGRAMA ALMACENAMIENTO\n⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘\nBIENVENIDO AL PROGRAMA ALMACENAMIENTO\n⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘⌘","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
 
         do {
             menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Almacenamiento de nombres.\n   - Opción 2: Cerrar sistema.","⌘⌘⌘ALMACENAMIENTO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
@@ -28,22 +28,29 @@ public class Nombres{
 
     static void menu(int espacio) {
         String nombres[]=new String[espacio];
+        String nombre, todo="";
+        int ubic, valor=espacio;
 
         do {
             menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Ingresar nombre.\n   - Opción 2: Ver nombre.\n   - Opción 3: Ver todos los nombres.\n   - Opción 4: Menú anterior.","⌘⌘⌘ALMACENAMIENTO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
             switch (menu) {
                 case 1:
-                    almacenar();
+                    nombre=nuevo();
+                    ubic=ubicacion(valor);
+                    nombres[ubic]=nombre;
                     break;
                 case 2:
-                    dato1();
+                    ubic=lugar(valor);
+                    nombre=nombres[ubic];
+                    dato1(nombre,ubic);
                     break;
                 case 3:
-                    dato2();
+                    for (String arreglo : nombres) {
+                        todo+="\""+arreglo+"\" ";
+                    }
+                    JOptionPane.showMessageDialog(null,"Datos: "+todo,"⌘⌘⌘MOSTRAR TODOS LOS DATOS⌘⌘⌘",JOptionPane.INFORMATION_MESSAGE);
                     break;
-                case 4:
-                    JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
-                    break;
+                case 4: break;
                 default:
                     JOptionPane.showMessageDialog(null,"Opción incorrecta, vuelva a intentarlo.","❌❌❌ERROR❌❌❌",JOptionPane.ERROR_MESSAGE);
                     break;
@@ -51,15 +58,28 @@ public class Nombres{
         } while (menu!=4);
     }
 
-    static void almacenar() {
+    static String nuevo() {
+        String nombre;
 
+        nombre=JOptionPane.showInputDialog(null,"Ingresar nombre a guardar:","⌘⌘⌘GUARDAR NOMBRE⌘⌘⌘",JOptionPane.QUESTION_MESSAGE);
+        return (nombre);
     }
 
-    static void dato1() {
+    static int ubicacion(int max) {
+        int ubic=0;
 
+        ubic=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresar ubicación (valor entre 0 y "+(max-1)+") para guardar el nombre ingresado:","⌘⌘⌘GUARDAR NOMBRE⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
+        return (ubic);
     }
 
-    static void dato2() {
+    static int lugar(int max) {
+        int ubic=0;
 
+        ubic=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingresar la ubicación (valor entre 0 y "+(max-1)+") que desea ver:","⌘⌘⌘MOSTRAR DATO⌘⌘⌘",JOptionPane.QUESTION_MESSAGE));
+        return (ubic);
+    }
+
+    static void dato1(String dato, int numero) {
+        JOptionPane.showMessageDialog(null,"Nombre almacenado en la posición "+numero+": "+dato+".","⌘⌘⌘MOSTRAR DATO⌘⌘⌘",JOptionPane.INFORMATION_MESSAGE);
     }
 }
