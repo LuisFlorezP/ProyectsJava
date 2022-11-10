f5ac5409c3ac356bd7816136ee7e77f541eab36a
Luis Florez <braian1k3@gmail.com>
2022-11-09 22:44:56 -0500

    Generar una nueva opción para el promedio pero ya dejando al usuario ingresar la cantidad de notas.

--- Nota.java
+++ Nota.java
@@ -2,13 +2,13 @@ import javax.swing.JOptionPane;
 
 public class Nota{
     public static void main(String[] args) {
-        String resul1="", resul2="";
+        String resul1="", resul2="", resulN="";
         int menu;
 
         JOptionPane.showMessageDialog(null,"■━■━■━■━■━■━■━■━■━■━■\nBIENVENIDO AL PROGRAMA NOTA\n■━■━■━■━■━■━■━■━■━■━■","LUCHINI S.A.S",JOptionPane.INFORMATION_MESSAGE);
 
         do {
-            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Promedio de 2 notas.\n   - Opción 2: Promedio de 3 notas.\n   - Opción 3: Cerrar sistema.","■━■━■NOTA■━■━■",JOptionPane.QUESTION_MESSAGE));
+            menu=Integer.parseInt(JOptionPane.showInputDialog(null,"   - Opción 1: Promedio de 2 notas.\n   - Opción 2: Promedio de 3 notas.\n   - Opción 3: Promedio de notas indicadas.\n   - Opción 4: Cerrar sistema.","■━■━■NOTA■━■━■",JOptionPane.QUESTION_MESSAGE));
             switch (menu) {
                 case 1:
                     resul1=promedio1();
@@ -19,6 +19,10 @@ public class Nota{
                     JOptionPane.showMessageDialog(null,resul2,"■━■━■NOTA■━■━■",JOptionPane.INFORMATION_MESSAGE);
                     break;
                 case 3:
+                    resulN=promedioN();
+                    JOptionPane.showMessageDialog(null,resulN,"■━■━■NOTA■━■━■",JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                case 4:
                     JOptionPane.showMessageDialog(null,"Saliendo del sistema...","LUCHINI S.A.S",JOptionPane.WARNING_MESSAGE);
                     break;
                 default:
@@ -26,7 +30,7 @@ public class Nota{
                     break;
             }
 
-        } while(menu!=3);    
+        } while(menu!=4);    
     }
 
     static String promedio1() {
@@ -91,6 +95,14 @@ public class Nota{
         def=Math.round(def);
         def/=10;
         mensaje+=def+".";       
+
+        return (mensaje);
+    }
+
+    static String promedioN() {
+        String mensaje="";
+
+
 
         return (mensaje);
     }
