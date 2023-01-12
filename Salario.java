cdd9748f69bf7a22b70cd5af0b5b9e749d9ac03b
Luis Florez <braian1k3@gmail.com>
2023-01-12 15:32:54 -0500

    Subir archivo referente al salario de una persona con deducciones.

---
+++ Salario.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+import javafx.css.PseudoClass;
+
+public class Salario {
+    public static void main(String[] args) {
+        double salario, horas, valHora, salud, pension, total;
+
+        JOptionPane.showMessageDialog(null, "BIENVENIDO", "LUCHINI S.A.S", JOptionPane.INFORMATION_MESSAGE);
+        valHora = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresar valor de la hora:", "INGRESE SALARIO", JOptionPane.QUESTION_MESSAGE));
+        horas = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresar cantidad de horas:", "INGRESE SALARIO", JOptionPane.QUESTION_MESSAGE));
+        salario = valHora * horas;
+        salud = salario * 0.04;
+        pension = salario * 0.04;
+        total = salario - salud - pension;
+        JOptionPane.showMessageDialog(null, "Salario normal: $" + salario + ".\nDescuento salud: $" + salud + ".\nDescuento pensión: $" + pension + ".\nSalario final: $" + total + ".", "NÓMINA", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
